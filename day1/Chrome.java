a32b61ed750cdac208e4f2526974ad060e624588
saras1992 <saras.jas92@gmail.com>
2025-05-25 08:59:43 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float chromev= 91.01f;
+		 System.out.println(chromev);
+		 String developer = "Google";
+		 System.out.println(developer);
+		 boolean beta = false;
+		 System.out.println(beta);
+		 int releaseYear = 2008;
+		 System.out.println(releaseYear);
+		 char shortcutkey = 'C';
+		  System.out.println(shortcutkey);
+		
+
+	}
+
+}
---
+++ day1/FirstProgram001.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class FirstProgram001 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Program");
+
+	}
+
+}
